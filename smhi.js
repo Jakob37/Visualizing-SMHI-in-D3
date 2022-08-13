8c78ff0a92c09b7cbde16f41e17ea58b39670ae5
Jakob Willforss <jakob.willforss@hotmail.com>
2022-08-13 14:13:29 +0200

    Add a Sweden map to the chart

--- smhi.js
+++ smhi.js
@@ -8,6 +8,59 @@ const main = async () => {
     const svg = d3.select("#root");
     console.log(root);
 
+    // year
+    // month
+    // temp
+    // sd
+    // nbr
+
+    const dimensions = {
+        width: 500,
+        height: 300,
+    };
+
+    await addMap(svg, dimensions);
+    await addDots(svg, dimensions);
+};
+
+async function addMap(svg, dimensions) {
+    console.log("addMap");
+
+    const projection = d3
+        .geoMercator()
+        .center([0, 65]) // GPS of locsation to zoom on
+        .scale(500) // This is like the zoom
+        .translate([dimensions.width / 2, dimensions.height / 2]);
+
+    console.log(projection);
+
+    // Load external data and boot
+    const map_data = await d3.json(
+        "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+    );
+    console.log(map_data)
+
+    // Filter data
+    map_data.features = map_data.features.filter(function (d) {
+        // console.log(d.properties.name);
+        return d.properties.name == "Sweden";
+        // return d.properties.name == "France";
+    });
+
+    console.log('remaining features', map_data.features)
+
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(map_data.features)
+        .enter()
+        .append("path")
+        .attr("fill", "lightgray")
+        .attr("d", d3.geoPath().projection(projection))
+        .style("stroke", "none");
+}
+
+async function addDots(svg, dimensions) {
     const smhi_data = await d3.csv("data/parsed_smhi.csv");
     console.log(smhi_data[0]);
 
@@ -18,17 +71,6 @@ const main = async () => {
         left: 20,
         bottom: 50,
         right: 20,
-    };
-
-    // year
-    // month
-    // temp
-    // sd
-    // nbr
-
-    const dimensions = {
-        width: 500,
-        height: 300,
     };
 
     const maxTemp = Math.max(...july_data.map(({ temp }) => temp));
@@ -79,4 +121,4 @@ const main = async () => {
         .attr("cy", (d) => tempScale(d.temp))
         .attr("r", 2)
         .style("fill", "blue");
-};
+}
